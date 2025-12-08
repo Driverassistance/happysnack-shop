2d8eecbd52f0d48ba600f3bf73aa7df272cbd716
Ereke <you@example.com>
2025-12-08 12:51:46 +0500

    fix: use PORT env variable for Render

--- app/api_server.py
+++ app/api_server.py
@@ -118,4 +118,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    web.run_app(app, host='0.0.0.0', port=8080)
+    port = int(os.getenv('PORT', 8080))
+    web.run_app(app, host='0.0.0.0', port=port)
